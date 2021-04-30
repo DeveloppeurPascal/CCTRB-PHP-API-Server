d00c9cdbff1b79bf4bf4aecfe1265769046187ea
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-04-30 16:28:19 +0200

    structure de stockage modifiee

---
+++ src/_private/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+	if (! (defined("CCTRB") && (CCTRB == "D870DCC7-2666-46C5-A1ED-CDA9E7D6F71B"))) {
+		print("KO");
+		exit;
+	}
+	print("OK");
+
+	define("DB_HOST","localhost");
+	define("DB_NAME","cctrb");
+	define("DB_USER","root");
+	define("DB_PWD","");
